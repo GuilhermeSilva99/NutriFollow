0036419b775c5f3b6ff31f78a5880927e712e504
luisfrl <luisfernando.25@hotmail.com>
2022-04-02 20:43:53 -0300

    Implementado emissão de token

--- Nutri-follow-web/routes/api.php
+++ Nutri-follow-web/routes/api.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Validation\ValidationException;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +19,19 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::post('/sanctum/token', function (Request $request) {
+    $request->validate([
+        'email' => 'required|email',
+        'password' => 'required',
+    ]);
+
+    $usuario = User::where('email', $request->email)->first();
+
+    if (!$usuario || !Hash::check($request->password, $usuario->password)) {
+        return response()->json("Credências invalidas", 400);
+    }
+
+    return $usuario->createToken($usuario->nome)->plainTextToken;
 });
