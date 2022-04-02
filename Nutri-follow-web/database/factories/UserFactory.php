b74829186b32a45aa998a12cdb576aa66d066755
luisfrl <luisfernando.25@hotmail.com>
2022-04-02 20:11:48 -0300

    Implementado verificacao de cadastro aprovado

--- Nutri-follow-web/database/factories/UserFactory.php
+++ Nutri-follow-web/database/factories/UserFactory.php
@@ -30,7 +30,8 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
-            'tipo_usuario' => 1
+            'tipo_usuario' => 1,
+            'cadastro_aprovado' => true
         ];
     }
 
